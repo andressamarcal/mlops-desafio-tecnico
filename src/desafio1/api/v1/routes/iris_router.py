b1d377805deac9d5116c0f802d34adbdc72ed070
Andressa Marçal <andressagb@gmail.com>
2024-06-24 19:02:49 -0300

    apply best practices and comments

---
+++ src/desafio1/api/v1/routes/iris_router.py
@@ -0,0 +1,60 @@
+from fastapi import APIRouter, HTTPException, Request
+from pydantic import BaseModel
+from sklearn.pipeline import Pipeline
+
+app_iris_predict_v1 = APIRouter()
+
+
+class IrisPredictionRequest(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+
+class IrisPredictionResponse(BaseModel):
+    prediction: int
+    probability: float
+
+
+def get_model(request: Request) -> Pipeline:
+    """
+    Obtém o modelo treinado do estado da aplicação.
+
+    Args:
+        request (Request): A requisição atual.
+
+    Returns:
+        Pipeline: O modelo treinado.
+    """
+    return request.app.state.model
+
+
+@app_iris_predict_v1.post(
+    "/iris/predict",
+    tags=["Predictions"],
+    response_model=IrisPredictionResponse,
+    description="Obtenha uma classificação para flores de Íris",
+)
+async def get_prediction(request: IrisPredictionRequest, req: Request):
+    """
+    Endpoint para obter previsões das espécies de flores Íris a partir das características da flor.
+
+    Args:
+        request (IrisPredictionRequest): Objeto contendo características de uma flor Íris.
+        req (Request): A requisição atual para obter o modelo treinado.
+
+    Returns:
+        IrisPredictionResponse: Um objeto contendo a previsão e a probabilidade.
+    """
+    model = get_model(req)
+    try:
+        data = [[request.sepal_length, request.sepal_width, request.petal_length, request.petal_width]]
+        prediction = model.predict(data)[0]
+        probability = max(model.predict_proba(data)[0])
+        return IrisPredictionResponse(prediction=prediction, probability=probability)
+    # especificar exceções mais detalhadas se torna melhor p/ compreensão dos logs e auditoria.
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"Erro de valor: {e}") from e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro do servidor: {e}") from e
