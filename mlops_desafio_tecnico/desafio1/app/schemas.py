7d2049a548d52a1d58d411b9bccb5b963a9c6865
Andressa Marçal <andressagb@gmail.com>
2024-06-24 16:24:05 -0300

    add docstring to schemas file

--- mlops_desafio_tecnico/desafio1/app/schemas.py
+++ mlops_desafio_tecnico/desafio1/app/schemas.py
@@ -4,12 +4,12 @@ from pydantic import BaseModel
 
 
 class IrisPredictionResponse(BaseModel):
-    """Schema para a resposta da previ
+    """Schema para a resposta da previsão.
 
     Attributes:
-        prediction (List[int]): Lista
-        probability (List[List[float]]
-        log_probability (List[List[flo
+        prediction (List[int]): Lista de previsões de classe para cada instância de entrada.
+        probability (List[List[float]]): Lista de probabilidades associadas a cada classe para cada instância de entrada.
+        log_probability (List[List[float]]): Lista de log probabilidades associadas a cada classe para cada instância de entrada.
     """
 
     prediction: List[int]
